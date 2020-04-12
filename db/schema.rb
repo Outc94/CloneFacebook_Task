5a7c2705140250916cb8d8a2b97bcbf74f85d64d
Mouhamed Diop <outc94@hotmail.fr>
2020-04-13 01:28:52 +0900

    First version of the Facebook clone with the Users, Sessions and Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_100419) do
+ActiveRecord::Schema.define(version: 2020_04_12_103451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 2020_04_12_100419) do
   create_table "users", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posts", "users"
