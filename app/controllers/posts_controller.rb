d884d0f990fb45c2b3778655dd76f49803c413e5
Mouhamed Diop <outc94@hotmail.fr>
2020-04-13 15:54:19 +0900

    Facebook Clone App

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
       if @post.save
-        redirect_to posts_path, notice: "Post Created！"
+        redirect_to posts_path, notice: "Post was created SUCCESSFULLY."
       else
         render :new
       end
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: 'Post was updated SUCCESSFULLY.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Post was deleted SUCCESSFULLY.' }
       format.json { head :no_content }
     end
   end
